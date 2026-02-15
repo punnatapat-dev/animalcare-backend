6a02664cc3a12b2eaa3b62e1f6fab081cac87fbe
punnatapat <punnatapat02@gmail.com>
2026-02-15 17:56:07 +0100

    feat: add pagination and update readme

--- backend/settings.py
+++ backend/settings.py
@@ -128,4 +128,6 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 6,  # เลข 6 คือจะให้โชว์หน้าละกี่ตัว (เปลี่ยนได้ตามใจชอบ)
 }
