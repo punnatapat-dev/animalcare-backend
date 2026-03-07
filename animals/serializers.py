4f38a0b032a6d38dc0edeec6a00933dc25971d33
punnatapat <punnatapat02@gmail.com>
2026-03-07 15:46:57 +0100

    add backend validation for animal name

--- animals/serializers.py
+++ animals/serializers.py
@@ -13,3 +13,12 @@ class AnimalSerializer(serializers.ModelSerializer):
         model = Animal
         fields = "__all__"
         read_only_fields = ("image_url", "image_public_id", "owner")
+
+    def validate_name(self, value):
+        if not value:
+            raise serializers.ValidationError("Name is required")
+
+        if len(value) < 2:
+            raise serializers.ValidationError("Name must be at least 2 characters")
+
+        return value
